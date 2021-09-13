a7c4851254a6eb738be9fa9ea65c7e6d4aeb5ac0
unknown <ashleyalam33@gmail.com>
2021-09-14 02:53:04 +0600

    final commit

--- js/app.js
+++ js/app.js
@@ -8,22 +8,30 @@ const loadProducts = () => {
 const showProducts = (products) => {
     const allProducts = products.map((pd) => pd);
     for (const product of allProducts) {
-        const image = product.images;
+        const image = product.image;
+        //product details
+        const { rate, count } = product.rating
         const div = document.createElement("div");
         div.classList.add("product");
         div.innerHTML = `<div class="single-product">
-      <div>
-    <img class="product-image" src=${image}></img>
-      </div>
-      <h3>${product.title}</h3>
-      <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
-      `;
+    <div>
+  <img class="product-image" src=${image}></img>
+    </div>
+    <h3>${product.title}</h3>
+    <p>Category: ${product.category}</p>
+    <h2>Price: $ ${product.price}</h2>
+    <h5> <i class="fas fa-users"></i> ${count}</h5>
+    <h5> <i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i>  ${rate}</h5>
+    <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
+    <button id="details-btn" class="btn btn-danger">Details</button></div>
+    `;
         document.getElementById("all-products").appendChild(div);
     }
 };
+
+
+
+//my cart calculation
 let count = 0;
 const addToCart = (id, price) => {
     count = count + 1;
@@ -31,11 +39,12 @@ const addToCart = (id, price) => {
 
     updateTaxAndCharge();
     document.getElementById("total-Products").innerText = count;
+    updateTotal();
 };
 
 const getInputValue = (id) => {
     const element = document.getElementById(id).innerText;
-    const converted = parseInt(element);
+    const converted = parseFloat(element);
     return converted;
 };
 
@@ -44,7 +53,8 @@ const updatePrice = (id, value) => {
     const convertedOldPrice = getInputValue(id);
     const convertPrice = parseFloat(value);
     const total = convertedOldPrice + convertPrice;
-    document.getElementById(id).innerText = Math.round(total);
+    console.log(total, typeof total)
+    document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
@@ -74,6 +84,6 @@ const updateTotal = () => {
     const grandTotal =
         getInputValue("price") + getInputValue("delivery-charge") +
         getInputValue("total-tax");
-    document.getElementById("total").innerText = grandTotal;
+    document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
 loadProducts();
